9901c3b1af13eeaafe93291d940e8c312d8eb848
Anderson <mr.anderson.brito@gmail.com>
2020-06-30 22:03:24 -0300

    Criado ChannelInfo component

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+  Container,
+  HashtagIcon,
+  Title,
+  Separator,
+  Description,
+} from "./styles";
+
+const ChannelInfo: React.FC = () => (
+  <Container>
+    <HashtagIcon />
+
+    <Title>Chat-Livre</Title>
+
+    <Separator />
+
+    <Description>Canal aberto para conversas</Description>
+  </Container>
+);
+export default ChannelInfo;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,13 @@ import { Grid } from "./styles";
 
 import ServerList from "../ServerList";
 import ServerName from "../ServerName";
+import ChannelInfo from "../ChannelInfo";
 
 const Layout: React.FC = () => (
   <Grid>
     <ServerList />
     <ServerName />
+    <ChannelInfo />
   </Grid>
 );
 export default Layout;
