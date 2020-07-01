a2d633b7e7108d12a25acccc3712eb8f22d17425
Anderson <mr.anderson.brito@gmail.com>
2020-07-01 19:38:57 -0300

    Criado UserInfo component

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Container,
+  Profile,
+  Avatar,
+  UserData,
+  Icons,
+  MicIcon,
+  HeadphoneIcon,
+  SettingsIcon,
+} from "./styles";
+
+const UserInfo: React.FC = () => (
+  <Container>
+    <Profile>
+      <Avatar />
+      <UserData>
+        <strong>HaiGo Studios</strong>
+        <span>#345</span>
+      </UserData>
+    </Profile>
+
+    <Icons>
+      <MicIcon />
+      <HeadphoneIcon />
+      <SettingsIcon />
+    </Icons>
+  </Container>
+);
+export default UserInfo;
